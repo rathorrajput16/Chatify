8ac53e6b19d4a5c2704a3699ea633c31d19cdfdf
Ayush <rathorrajput167@gmail.com>
2025-12-17 16:34:44 +0530

    EMAIL SENT

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import { generateToken } from "../lib/utilis.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-
+import "dotenv/config";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 
 export const signup=async (req, res) => {
@@ -28,14 +29,22 @@ export const signup=async (req, res) => {
       password: hashedPassword,
     });
      if(newUser){
-       generateToken(newUser._id,res)
-       await newUser.save();
+    //    generateToken(newUser._id,res)
+    //    await newUser.save();
+    const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
        res.status(201).json({
         _id:newUser._id,
         fullName:newUser.fullName,
         email:newUser.email,
         profilePic:newUser.profilePic,
-       })
+       });
+       try {
+        await sendWelcomeEmail(savedUser.email, savedUser.fullName, process.env.CLIENT_URL);
+      } catch (error) {
+        console.error("Failed to send welcome email:", error);
+      }
      }else{
       res.status(400),json({message:"Error creating user"})
      }
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 export const connectDB=async()=>{
     try {
+        const{MONGO_URI}=process.env;
+        if(!MONGO_URI) throw new Error("MONGO URI NOT SET");
         const conn=await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected ",conn.connection.host)
     } catch (error) {
