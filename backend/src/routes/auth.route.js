1a2f378f14b232ea52a6a5040bd8b7a9a05cbab6
Ayush <rathorrajput167@gmail.com>
2025-12-17 18:43:27 +0530

    message routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,7 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
 import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 const router = express.Router();
-router.use(arcjetProtection);
+// router.use(arcjetProtection);
 router.post("/signup",signup);
 
 router.post("/login", login);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,14 @@
 import express from 'express';
 const router = express.Router();
+import { getAllContacts,getMessagesByUserId,sendMessage,getChatPartners } from '../controllers/message.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 router.get("/send", (req, res) => {
   res.send('Send Message endpoint');
 });
+
+router.get("/contacts", protectRoute, getAllContacts);
+router.get("/chats",protectRoute, getChatPartners);
+ router.get("/:id", protectRoute,getMessagesByUserId);
+ router.post("/send/:id",protectRoute,sendMessage);
+
 export default router;
