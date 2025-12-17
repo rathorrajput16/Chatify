ce1e831d66e692aead24ad7875ec964471619e54
Ayush <rathorrajput167@gmail.com>
2025-12-17 18:14:15 +0530

    rate limit done

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 const router = express.Router();
+router.use(arcjetProtection);
 router.post("/signup",signup);
 
 router.post("/login", login);
